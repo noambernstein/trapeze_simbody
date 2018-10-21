17b8a22d07712f094f7264b3acc4ed256e7f0289
Noam Bernstein <noamb@bollweevil.gdbg.org>
2018-10-20 23:22:18 -0400

    fix net

--- make_mesh.py
+++ make_mesh.py
@@ -2,22 +2,20 @@
 
 import math
 
-length = 10.0
-n_length = 10
+length = 25.0
+n_length = 25
 width = 3.0
 n_width = 3
-r = 100.0
-z0 = None
+r = 200.0
+x0 = - length/2.0
+y0 = - width/2.0
+z0 = math.sqrt(r**2 - x0**2 - y0**2)
 for i_length in range(n_length+1):
     for i_width in range(n_width+1):
         x = length * float(i_length)/float(n_length) - length/2.0
         y = width * float(i_width)/float(n_width) - width/2.0
-        z = math.sqrt(r**2 - x**2 - y**2)
-        if z0 is None:
-            z0 = z
-        z = z0 - z
-        print "v", x,y,z
-
+        z = z0 - math.sqrt(r**2 - x**2 - y**2)
+        print "v", x,z,y
 
 for i_length in range(n_length):
     for i_width in range(n_width):
@@ -25,5 +23,5 @@ for i_length in range(n_length):
         i1 = i_length*(n_width+1)+i_width+1
         i2 = (i_length+1)*(n_width+1)+i_width+1
         i3 = (i_length+1)*(n_width+1)+i_width
-        print "f",i0,i1,i2
-        print "f",i0,i2,i3
+        print "f",i0+1,i1+1,i2+1
+        print "f",i0+1,i2+1,i3+1
